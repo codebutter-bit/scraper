18e9afc892b4418685d81c94305e2cd8cd4fd0e5
codebutter-bit <codebutter20@gmail.com>
2025-11-11 14:07:11 -0500

    first commit

---
+++ addlist.js
@@ -0,0 +1,62 @@
+const addonList = [
+  //   {
+  //   title: "Zilean-MM",
+  //   logo: "https://cdn-icons-png.flaticon.com/512/477/477256.png",
+  //   description: "Torrentio + MediaFusion + DMM",
+  //   link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/zilean.json",
+  // },
+    {
+    title: "Lightning Fast",
+    logo: "https://cdn-icons-png.flaticon.com/512/5346/5346469.png",
+    description: "Fastest results possible",
+    link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/LightningFast.json",
+  },
+     {
+    title: "Slow Burn",
+    logo: "https://cdn-icons-png.flaticon.com/512/10760/10760659.png",
+    description: "More results but not as fast",
+    link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/SlowBurn.json",
+  },
+  // {
+  //   title: "Ultra-Fast",
+  //   logo: "./images/ff.png",
+  //   description: "Torrentio + MediaFusion",
+  //   link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/addons-en.json",
+  // },
+  // {
+  //   title: "Torr-Med",
+  //   logo: "./images/candy-corn.png",
+  //   description: "Torrentio + MediaFusion",
+  //   link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/all-imdb-scrapers.json",
+  // },
+  // {
+  //   title: "BigFatty",
+  //   logo: "./images/burger.png",
+  //   description: "A lot of scrapers and a lot more sources!",
+  //   link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/all-in-one.json",
+  // },
+  {
+    title: "Spanish",
+    logo: "./images/spanish.png",
+    description: "Peerflix and Torrentio!",
+    link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/spanish.json",
+  }
+  // ,
+  // {
+  //   title: "Old-Timers",
+  //   logo: "./images/grandfather.png",
+  //   description:
+  //     "PirateBay + YTS + CloudTorrent + Lime-Torrents + Torrents-CSV + Bit-Search",
+  //   link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/en-scrapers.json",
+  // },
+  // {
+  //   title: "Just Torrentio",
+  //   logo: "./images/ok.png",
+  //   description: "Just Torrentio",
+  //   link: "https://raw.githubusercontent.com/codebutter-bit/scraper/refs/heads/main/torentio-en.json",
+  // },
+];
+
+
+
+
